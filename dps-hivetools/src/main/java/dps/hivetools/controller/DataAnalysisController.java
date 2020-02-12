bde8e3cbf3a0369d2741a8f9ceea761419e0dae7
EnergyConservation <chinabaotaotao@hotmail.com>
2020-02-12 20:38:29 +0800

    baotaotao:2038

--- dps-hivetools/src/main/java/dps/hivetools/controller/DataAnalysisController.java
+++ dps-hivetools/src/main/java/dps/hivetools/controller/DataAnalysisController.java
@@ -21,6 +21,10 @@ public class DataAnalysisController {
         if (file == null) {
             return "请上传文件！";
         }
+        if (!file.getContentType().equals("application/vnd.ms-excel") &&
+                !file.getContentType().equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+            return "请上传Excel文件！";
+        }
         String fileName = file.getOriginalFilename();
         log.info(String.format("开始解析Excel [%s] 文件名：[%s] 文件大小：[%d]", NDateUtil.getTime(), fileName, file.getSize()));
 
@@ -32,7 +36,7 @@ public class DataAnalysisController {
             log.error(String.format("文件：%s 解析失败！[%s] 业务异常错误信息：%s", fileName, NDateUtil.getTime(), ex.getErrorMsg()));
             return String.format("文件：%s 解析失败！[%s] 业务异常错误信息：%s", fileName, NDateUtil.getTime(), ex.getErrorMsg());
         } catch (Exception ex) {
-            log.error(String.format("文件：%s 解析失败！[%s] 未知异常错误信息：%s", fileName, NDateUtil.getTime(), ex.getMessage()));
+            log.error("error: {}", ex.getMessage(), ex);
             return String.format("文件：%s 解析失败！[%s] 未知异常错误信息：%s", fileName, NDateUtil.getTime(), ex.getMessage());
         }
 
